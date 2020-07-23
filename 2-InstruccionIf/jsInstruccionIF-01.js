19aeebb6949efce78ef2c6926bbcc396a7cd0aa3
EvelynYanez <evelyn.gabriela.yanez@gmail.com>
2020-07-23 18:03:03 -0300

    Correc de if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -3,11 +3,11 @@ function mostrar()
 	//tomo la edad  
 	var vEdad;
 
-	vEdad = document.getElementById("txtIdEdad").value;
+	vEdad = parseInt(document.getElementById("txtIdEdad").value);
 
 	// Si es  igual a 15 muestro el mensaje
 
-	if (vEdad == "15"){
+	if (vEdad == 15){
 		alert("niña bonita");
 	}
 
